62c41dafd7823872e55a3a40d6af479e009c7acf
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-06 18:30:29 -0400

    updated dashboard component to include CreateQuestion and QuestionList components

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CreateQuestionComponent } from './../create-question/create-question.component';
+import { QuestionListComponent } from './../question-list/question-list.component';
+
 @Component({
     moduleId: module.id,
     selector: 'dashboard',
-    templateUrl: 'dashboard.component.html'
+    templateUrl: 'dashboard.component.html',
+    directives: [
+        CreateQuestionComponent,
+        QuestionListComponent
+    ]
 })
 export class DashboardComponent implements OnInit {
     
