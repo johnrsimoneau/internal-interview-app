e859be5f9c84406c3c8b86498c5acd2f165b9112
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-06 18:24:09 -0400

    question-list component files added and modified the dashboard for question-list and create-question components

---
+++ app/question-list/question-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'question-list',
+    templateUrl: 'question-list.component.html'
+})
+export class QuestionListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
