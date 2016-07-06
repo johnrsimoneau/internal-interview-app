fabc437e266c2389e97dd2997b6bae9468a5f454
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-06 18:20:49 -0400

    modified package.json to include bootstrap; added create-question structure; and dashboard component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>Cardinal Solutions Interview App</h1>'
+    selector: 'interview-app',
+    template: `
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>{{title}}</h1>
+            </div>
+        </div>
+        <dashboard></dashboard>
+    `,
+    directives: [DashboardComponent]
 })
-export class AppComponent { }
+export class AppComponent { 
+    title: string = "Cardinal Solutions Interview App";
+}
