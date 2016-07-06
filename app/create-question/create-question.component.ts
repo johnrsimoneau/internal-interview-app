90e5b78493edf58c7ce9505dc283dd481dbceec4
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-06 18:25:25 -0400

    create-question component content added

---
+++ app/create-question/create-question.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'create-question',
+    templateUrl: 'create-question.component.html'
+})
+export class CreateQuestionComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
