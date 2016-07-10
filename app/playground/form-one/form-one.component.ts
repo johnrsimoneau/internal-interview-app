459614d92f0a0d13def4eddce4ebeb986d262b0c
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-10 06:08:06 -0400

    using FormBuilder directive

--- app/playground/form-one/form-one.component.ts
+++ app/playground/form-one/form-one.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    FORM_DIRECTIVES,
+    REACTIVE_FORM_DIRECTIVES,
+    FormBuilder,
+    FormGroup
+} from '@angular/forms';
 
 @Component({
     moduleId: module.id,
     selector: 'form-one',
-    templateUrl: 'form-one.component.html'
+    templateUrl: 'form-one.component.html',
+    directives: [
+        FORM_DIRECTIVES,
+        REACTIVE_FORM_DIRECTIVES,
+    ]
 })
 export class FormOneComponent implements OnInit {
-    
+
+    myForm: FormGroup;
+
+    constructor(fb: FormBuilder) {
+        this.myForm = fb.group({
+            'sku': ['ABC123']
+        });
+    }
+
     onSubmit(form: any) {
         console.log('You submitted:', form);
     }
-
-    constructor() { }
-
+    
     ngOnInit() { }
 
 }
