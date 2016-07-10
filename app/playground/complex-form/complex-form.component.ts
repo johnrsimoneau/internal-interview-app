4e9f2ae8870c83992fd70523981ef3f758a66ac4
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-10 14:48:37 -0400

    added complex form component to playground

---
+++ app/playground/complex-form/complex-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'complex-form',
+    templateUrl: 'complex-form.component.html'
+})
+export class ComplexFormComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
--- app/playground/playground.component.ts
+++ app/playground/playground.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FormOneComponent } from './example-form/form-one.component';
+import { ComplexFormComponent } from './complex-form/complex-form.component';
 
 @Component({
     moduleId: module.id,
     selector: 'playground',
     templateUrl: 'playground.component.html',
     directives: [
-        FormOneComponent
+        FormOneComponent,
+        ComplexFormComponent
     ]
 })
 export class PlaygroundComponent implements OnInit {
