8b9bcd8d8b9f8400d2cd7de87f0f1d0486f05cff
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-10 19:21:19 -0400

    working wiht required fields

--- app/playground/complex-form/complex-form.component.ts
+++ app/playground/complex-form/complex-form.component.ts
@@ -1,12 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+    FORM_DIRECTIVES,
+    REACTIVE_FORM_DIRECTIVES,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+    AbstractControl
+} from '@angular/forms';
+
+import { LastNameInputComponent } from './last-name-input/last-name-input.component';
+
+// functions for form validation
+function firstNameValidator(control: FormControl): { [s:string]:boolean } {
+    if(control.value.length < 2) {
+        return { invalidFirstNameLength: true };
+    }
+}
+
 @Component({
     moduleId: module.id,
     selector: 'complex-form',
-    templateUrl: 'complex-form.component.html'
+    templateUrl: 'complex-form.component.html',
+    directives: [
+        FORM_DIRECTIVES,
+        REACTIVE_FORM_DIRECTIVES,
+        LastNameInputComponent
+    ]
 })
 export class ComplexFormComponent implements OnInit {
-    constructor() { }
+
+    complexForm: FormGroup;
+    firstName: AbstractControl;
+    lastName: AbstractControl;
+
+    clickedSubmit = false;
+
+    constructor(fb: FormBuilder) {
+
+        this.complexForm = fb.group({
+            'firstName': ['', Validators.compose([
+                firstNameValidator,
+                Validators.required
+            ])],
+            'lastName': ['', Validators.required]
+        });
+
+        this.firstName = this.complexForm.controls['firstName'];
+        this.lastName = this.complexForm.controls['lastName'];
+        
+    }
+
+    onSubmit(form: any) {
+        if (this.complexForm.valid) {
+            console.log('You submitted ', form);
+        } else {
+            this.clickedSubmit = true;
+            return;
+        }
+    }
 
     ngOnInit() { }
 
---
+++ app/playground/complex-form/last-name-input/last-name-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {
+    FORM_DIRECTIVES,
+    REACTIVE_FORM_DIRECTIVES,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+    AbstractControl
+} from '@angular/forms';
+
+@Component({
+    moduleId: module.id,
+    selector: 'last-name-input',
+    templateUrl: 'last-name-input.component.html'
+})
+export class LastNameInputComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
