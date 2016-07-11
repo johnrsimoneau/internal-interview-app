649b31e0fdbf73eea03513e9e71d55e8fb7cfcad
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-11 17:46:41 -0400

    searching for tags dynamically using Observables

--- app/create-question/tags/tag.service.ts
+++ app/create-question/tags/tag.service.ts
@@ -1,21 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-
-const URL_TAGS = 'http://localhost:1313/api/questionTags';
+// import 'rxjs/add/operator/map;
 
 @Injectable()
 export class TagService {
 
     constructor(private _http: Http) { }
 
-    getTags() {
-        return this._http.get(URL_TAGS) 
-            .map((response: Response) => response.json())
-            .catch(this._handleError);
+    private _extractData(res: Response) {
+        let body = res.json();
+        return body || [];
     }
 
-    _handleError(err:any) {
+    searchTags(term:string): Observable<string[]> {
+        let apiUrl = "http://localhost:1313/api/"
+        let params: string = [
+            `questionTags`,
+            `search`,
+            `${term}`
+        ].join('/');
+        let queryUrl: string = apiUrl + params;
+
+        return this._http.get(queryUrl)
+            .map(this._extractData)
+            .catch(this.handleError);
+
+    }
+
+    /* PROMISE BASED CALL
+    searchTags(term:string) {
+        let apiUrl = "http://localhost:1313/api/"
+        let params: string = [
+            `questionTags`,
+            `search`,
+            `${term}`
+        ].join('/');
+        let queryUrl: string = apiUrl + params;
+
+        return this._http
+            .get(queryUrl)
+            .toPromise()
+            .then(this._extractData)
+    }
+    */
+
+    handleError(err:any) {
         console.log(err);
         // TODO: customize the message to be user-friendly
         return Observable.throw(err); 
--- app/create-question/tags/tags.component.ts
+++ app/create-question/tags/tags.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Observable } from 'rxjs/Rx';
 import { TagService } from './tag.service';
+import { ObjectToArrayPipe } from './../../pipes/objectToArray.pipe';
 
 @Component({
     moduleId: module.id,
     selector: 'tags',
-    templateUrl: 'tags.component.html'
+    templateUrl: 'tags.component.html',
+    pipes: [ObjectToArrayPipe]
 })
 export class TagsComponent implements OnInit {
+    availableTags: Array<string>;
+    @Input() selectedTags:any[] = [];
 
-    @Input() selectedTags:string[] = [];
+    //availableTags: Observable<string[]>;
 
-    availableTags: any[];
+    constructor(private _tagService: TagService) { }
 
+    searchTags(term: string) {
+        this._tagService
+            .searchTags(term)
+            .filter(text => term.length >= 3)
+            .debounceTime(400)
+            .distinctUntilChanged()
+            .subscribe(
+                availableTags => this.availableTags = availableTags
+            )
+            
+            // .then(availableTags => this.availableTags = availableTags);
+    }
     addTag(id:string, tag:string, count:number ) {
         var object:any = {"_id": id, "tag": tag, "count": count };
         this.selectedTags.push(object);
@@ -23,14 +39,6 @@ export class TagsComponent implements OnInit {
         this.selectedTags.splice(index, 1);
     }
 
-    constructor(private _tagService: TagService) { }
-
-    ngOnInit() {
-        this._tagService.getTags()
-            .subscribe(
-                (availableTags) => this.availableTags = availableTags,
-                (err) => { console.log(err); }
-            )
-    }
+    ngOnInit() {}
 
 }
