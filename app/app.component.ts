75cc0b79ae40f74e7777a1d641d45772781452ed
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-14 08:58:14 -0400

    updated nav bar to use bootstrap's nav

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,19 +13,27 @@ import { QuestionListService } from './question-list/question-list.service';
 @Component({
     selector: 'interview-app',
     template: `
-        <div class="row">
-            <div class="col-sm-12">
-                <h1 class="page-header">{{title}}</h1>
+        <div class="navbar navbar-inverse">
+            <div class="container">
+                <ul class="nav navbar-nav">
+                    <li><a [routerLink]="['question-list']">Questions</a></li>
+                    <li><a [routerLink]="['create-question']">Create Question</a></li>
+                    <li><a [routerLink]="['playground']">Playground</a></li>
+                </ul>
             </div>
         </div>
-        <div class="nav">
-            <ul>
-                <li><a [routerLink]="['question-list']">Questions</a></li>
-                <li><a [routerLink]="['create-question']">Create Question</a></li>
-                <li><a [routerLink]="['playground']">Playground</a></li>
-            </ul>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="page-header">{{title}}</h1>
+                </div>
+            </div>
         </div>
-        <router-outlet></router-outlet>
+
+        <div class="container">
+            <router-outlet></router-outlet>
+        </div>
     `,
     directives: [ROUTER_DIRECTIVES],
     providers: [QuestionListService]
