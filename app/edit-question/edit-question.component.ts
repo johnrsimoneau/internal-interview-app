055395382e76a0f48165b540f7ecad66e48a4869
johnrsimoneau <johnrsimoneau@gmail.com>
2016-07-16 05:47:06 -0400

    Added edit question contents

---
+++ app/edit-question/edit-question.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'edit-question',
+    templateUrl: 'edit-question.component.html'
+})
+export class EditQuestionComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
